813cbebbf2d529ed33d838e22e5e37b0849ec898
Nafees Siddiqui <106441992+Nafeessidd1@users.noreply.github.com>
2024-05-27 01:13:23 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -49,10 +49,10 @@ export const Projects = () => {
         <Row>
           <Col size={12}>
                 <h2>Projects</h2>
-                <p> 1. Developed a dynamic Ball Shooting Game using HTML, CSS, and JavaScript, showcasing interactive web design.<br>
-                    2. Developed a smart surveillance system for CCTV that triggers an alarm upon detecting unauthorized entry into restricted areas and uses real-time Firebase for facial recognition.<br>
-                    3. Crafted an E-commerce Website with HTML, CSS, JavaScript, and React for a seamless user experience.<br>
-                    4. Developed a real-time voting system using Apache Kafka, Apache Spark, PostgreSQL, Streamlit, and Docker.<br>
+                <p> 1. Developed a dynamic Ball Shooting Game using HTML, CSS, and JavaScript, showcasing interactive web design.<br/>
+                    2. Developed a smart surveillance system for CCTV that triggers an alarm upon detecting unauthorized entry into restricted areas and uses real-time Firebase for facial recognition.<br/>
+                    3. Crafted an E-commerce Website with HTML, CSS, JavaScript, and React for a seamless user experience.<br/>
+                    4. Developed a real-time voting system using Apache Kafka, Apache Spark, PostgreSQL, Streamlit, and Docker.<br/>
                     5. Implemented a Machine Learning project for house prediction, combining coding skills with data analysis and model development.
                 </p>
 
